d4fffe96e7cc1cf21d48a6ec6a07fa8e2b4828af
Charlie <sketchblock@gmail.com>
2020-11-19 13:27:50 -0800

    Added error handling and refactored

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -3,43 +3,85 @@ import Search from '../search';
 import SearchItems from '../search-items';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import Pagination from '@material-ui/lab/Pagination';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import githubService from '../../services/githubService';
 
 const Main = () => {
     const [searchText, setSearchText] = useState("");
     const [searchItems, setSearchItems] = useState([]);
-    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
     const [pages, setPages] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const searchUserRepositories = (event, page = 1) => {
         if (!searchText) return;
 
+        setIsError(false);
+        setErrorMessage("");
+        setLoading(true);
+
         githubService.searchUserRepositories(searchText, page, itemsPerPage)
             .then(response => {
+                const { errors, message } = response;
+                if (errors || message) {
+                    setIsError(true);
+                    setErrorMessage(
+                        errors ? errors[0].message : message
+                    );
+                };
+
                 const { items, total_count } = response;
                 const pages = Math.floor(total_count / itemsPerPage);
-                console.log(response);
+
                 setSearchItems(items);
                 setPages(pages);
-            })
+                setLoading(false);
+            });
+    };
+
+    const displaySearchItems = () => {
+        if (loading) {
+            return <CircularProgress />;
+        } else if (isError) {
+            return (
+                <Box mt="15px">
+                    <Typography>{ errorMessage }</Typography>
+                </Box>
+            );
+        } else {
+            return <SearchItems searchItems={searchItems} />;
+        }
+    };
+
+    const displayPagination = () => {
+        if (pages) {
+            return (
+                <Pagination
+                    color="primary"
+                    count={pages}
+                    onChange={handlePageChange} />
+            );
+        } else {
+            return null;
+        }
     };
 
     const handleKeyPress = event => {
         if (event.key === "Enter") searchUserRepositories();
-    }
-
-    const handlePageChange = (event, value) => {
-        searchUserRepositories(event, value);
     };
+
+    const handlePageChange = (event, value) => searchUserRepositories(event, value);
 
     return (
         <Container maxWidth="sm">
             <Box mt="100px">
-                <Box display="flex" justifyContent="center">
-                    <div>hello world!</div>
+                <Box mb="20px" display="flex" justifyContent="center" textAlign="center">
+                    <Typography variant="h3">GitHub User Repo Search</Typography>
                 </Box>
                 <Box display="flex" justifyContent="center">
                     <Search
@@ -48,19 +90,11 @@ const Main = () => {
                         searchUserRepositories={searchUserRepositories} 
                         handleKeyPress={handleKeyPress} />
                 </Box>
-                <Box>
-                    <SearchItems 
-                        searchItems={searchItems} />
+                <Box display="flex" justifyContent="center">
+                    { displaySearchItems() }
                 </Box>
                 <Box display="flex" justifyContent="center">
-                    {
-                        pages
-                            ? <Pagination
-                                color="primary"
-                                count={pages}
-                                onChange={handlePageChange} />
-                            : null
-                    }
+                    { displayPagination() }
                 </Box>
             </Box>
         </Container>
--- src/components/search-items/index.js
+++ src/components/search-items/index.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+
+const TitleAndStars = ({ fullName, stars }) => (
+    <Box display="flex" justifyContent="space-between">
+        <Box>{ fullName }</Box>
+        <Box display="inline-block">
+            <Box component="span" mr="5px">{ stars }</Box>
+            <svg aria-label="star" className="octicon octicon-star" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path fillRule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
+        </Box>
+    </Box>
+);
+
+const Description = ({ description }) => <Typography component="span">{ description }</Typography>;
 
 const mapSearchItems = item => {
-    const { id, full_name, html_url } = item;
+    const { id, full_name, html_url, description, stargazers_count } = item;
     return (
-        <ListItem key={id} button component="a" href={html_url}>
-            <ListItemText 
-                primary={full_name}
-            />
-        </ListItem>
-    )
-}
+        <React.Fragment key={id} >
+            <ListItem button component="a" href={html_url}>
+                <ListItemText 
+                    primary={ <TitleAndStars fullName={full_name} stars={stargazers_count}/>}
+                    secondary={ <Description description={description} /> } />
+            </ListItem>
+            <Divider />
+        </React.Fragment>
+    );
+};
 
-
-const SearchItems = ({ searchItems }) => (
-    <List>
-        { searchItems.map(mapSearchItems) }
-    </List>
-)
+const SearchItems = ({ searchItems }) => <List>{ searchItems.map(mapSearchItems) }</List>;
 
 SearchItems.propTypes = {
     searchItems: PropTypes.array.isRequired
-}
+};
 
 export default SearchItems;
